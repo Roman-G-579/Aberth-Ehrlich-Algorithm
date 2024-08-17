a25b1377da39106ec8590dd7fe32fa239f8de67c
Roman-G-579 <romangury579@gmail.com>
2024-08-17 10:13:19 +0300

    Finished algorithm implementations

--- aberthEhrlich.rkt
+++ aberthEhrlich.rkt
@@ -2,35 +2,30 @@
 
 (define pi 3.141592653589793)
 
-(define (round-complex complex-nums)
-  (map (λ(num)
-         (let ((real (real-part num))
-               (imag (imag-part num)))
-           (+ (/ (round (* real 1000)) 1000)
-              (* (/ (round (* imag 1000)) 1000) 0+i))))
-       complex-nums))
 
 ; Reads the file line-by-line, trims each line and converts the string to a number,
 ; that is added to a list
 ; Once end-of-file is reached, the list is returned
 (define (read-coefficients file)
-  (let ((line (read-line file 'any)))
+  (let ((line (read-line file)))
     (if (eof-object? line)
         '()
          (cons (string->number (string-trim line)) (read-coefficients file)))))
 
-
+; Checks if a complex number is close to zero within a given epsilon
 (define (is-close-to-zero value epsilon)
   (and (< (abs (real-part value)) epsilon)
        (< (abs (imag-part value)) epsilon)))
 
-
-
- (define poly-val (λ(p x)
+; Evaluates a polynomial at a given value x
+; a_0+a_1x+...+a_nx^n = a0 + x(a_1+x(a_2+x(a_3+...+x(a_(n-1)+xa_n)...)))
+(define poly-val (λ(p x)
          (if (null? p)
-             0
+             0 ; Base case: if the list of coefficients is empty, return 0
+             ; Otherwise, take the first coefficient, multiply it by x and add the result of evaluating the rest of the polynomial
              (+ (car p) (* x (poly-val (cdr p) x))))))
 
+; Evaluates a rational function p(x)/q(x) at x
 (define (frac-val p q x)
   (if (<= (magnitude x) 1)
       (/ (poly-val (reverse p) x) (poly-val (reverse q) x))
@@ -52,6 +47,7 @@
               (loop (+ i 1) (cons (* (list-ref coefficients i) (- n i 1)) result))))))) ; Calculates the coeff of the i-th term
               ; [4x^2,2x,1] -> 4x2+2x1 -> [8,2]
 
+; Generates initial approximations for the polynomial roots
 (define (get-approximations coefficients)
   (let* ((deg (- (length coefficients) 1))
          (p-0 (list-ref coefficients deg))
@@ -59,19 +55,19 @@
          (r (magnitude (expt (/ p-0 p-n) (/ 1 deg)))) ; r = (|p0/pn|)^(1/deg)
 
          ;Distributes angles evenly, the angle amount is equal to the degree of the polynomial
-         (angles (for/list ((x (in-range 0 deg))) (* x (/ (* 2 pi) deg)))))
+         (angles (for/list ((x (in-range deg))) (* x (/ (* 2 pi) deg))))) ; Computes the angles for the approximations
     (map (λ(angle) (+ (* r (cos angle)) (* r (sin angle) 0+i))) angles))) ; Converts the polar coordinates to complex numbers
 
 
 ; Calculates the distance from approximation k to every other approximation in the list
-(define (calculate-sigma approximations k)
-  (let ((zk (list-ref approximations k)))
-    (foldl (λ(zj sigma)
-             (if (not (= zj zk))
-                 (+ sigma (/ 1 (- zk zj)))
-                 sigma))
-           0.0
-           approximations)))
+(define (calculate-sigma approximations zk)
+  (let loop ((lst approximations) (sigma 0.0))
+      (cond
+        ((null? lst) sigma) ; If the list is empty, return the accumulated sigma
+        ((not (= (car lst) zk)) ; If the current element is not equal to zk
+         (loop (cdr lst) (+ sigma (/ 1 (- zk (car lst)))))) ; Update sigma and continue with the rest of the list
+        (else (loop (cdr lst) sigma))))) ; If the current element equals zk, skip it and continue
+
 
 ; Check if all given approximations count as converged for the given epsilon
 ; by plugging each approximation into the polynomial and evaluating the result's distance from the epsilon
@@ -87,21 +83,21 @@
         (cond
           ((null? lst) #t) ; If the list is empty, all elements were close to zero
           ((not (is-close-to-zero (car lst) epsilon)) #f) ; If an element is not close to zero, return #f
-          (else (loop (cdr lst)))))) ; Otherwise, continue with the rest of the list
-)
+          (else (loop (cdr lst))))))) ; Otherwise, continue with the rest of the list
 
+; Implements the Aberth method for finding polynomial roots
 (define (aberth-method coefficients epsilon)
   (let ((approximations (get-approximations coefficients))
          (derivative-coefficients (polynomial-derivative-coefficients coefficients))
-         (max-iterations 20))
+         (max-iterations 20)) ; Constant value for max iteration limit
 
         (define (iterate approximations iteration)
           (if (>= iteration max-iterations)
               approximations ; If max-iterations has been reached, the approximations list is returned
               (let ((offsets
-                     (for/list ((zk approximations) (k (in-naturals)))
+                     (for/list ((zk approximations))
                        (let ((frac (frac-val coefficients derivative-coefficients zk))
-                              (sigma (calculate-sigma approximations k)))
+                              (sigma (calculate-sigma approximations zk)))
                          (/ frac (- 1 (* frac sigma))))))) ; offset = frac / ( 1 - (frac*sigma) )
 
              (let ((new-approximations (map - approximations offsets))) ;
@@ -118,16 +114,19 @@
 ; Opens an input file (calls open-input-file), and calls read-coefficients using it as a parameter
 (define coefficients (call-with-input-file "poly_coeff(997).txt" read-coefficients))
 
+
 (define result (time (aberth-method coefficients 0.0001)))
 
 
-;(printf "Coefficients: ~a\n" derivative-coefficients)
-;(printf "Roots: ~a\n" result)
-;(printf "Rounded Roots: ~a\n" (round-complex result))
-;(printf "len ~a\n" (length result))
-
-; Define the function to print each complex number on a new line
+; Prints each complex number in a new line
 (define (print-complex-list complex-list)
   (for-each (lambda (z) (printf "~a\n" z)) complex-list))
 
 (print-complex-list result)
+
+
+
+
+
+;(define test-val (poly-val (reverse coefficients) 0.7450737533631855-0.6700384058892166i))
+;(print test-val)
